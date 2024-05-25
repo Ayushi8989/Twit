34eb9d58ca0276d9417ecf19d70aa1fa6007a257
Ayushi <ayushi0628@gmail.com>
2024-05-26 04:05:08 +0530

    add: user-model, auth/signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,17 +1,69 @@
-export const signup = async (req, res)=>{
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateTokenandsetCookie } from "../lib/utils/generateToken.js";
+
+export const signup = async (req, res) => {
+    try {
+        const { fullName, email, password, username } = req.body;
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ error: "Invalid email format." });
+        }
+
+        const existingUser = await User.findOne({ username: username });
+        if (existingUser) {
+            return res.status(400).json({ error: "Username already taken." })
+        }
+
+        const existingEmail = await User.findOne({ email: email });
+        if (existingEmail) {
+            return res.status(400).json({ error: "Email already exists." })
+        }
+
+        //hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            username: username,
+            email: email,
+            fullName: fullName,
+            password: hashedPassword
+        })
+
+        //generating jwt token
+        if (newUser) {
+            generateTokenandsetCookie(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                username: newUser.username,
+                followers: newUser.followers,
+                following: newUser.following,
+                profileImg: newUser.profileImg,
+                coverImg: newUser.coverImg,
+            })
+        } else {
+            res.status(400).json({ error: "Invalid user data." });
+        }
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ error: "Internal server error." });
+    }
+}
+
+export const login = async (req, res) => {
     res.json({
-        data : "you hit the signup endpoint",
+        data: "you hit the login endpoint",
     });
 }
 
-export const login = async (req, res)=>{
+export const logout = async (req, res) => {
     res.json({
-        data : "you hit the login endpoint",
-    });
-}
-
-export const logout = async (req, res)=>{
-    res.json({
-        data : "you hit the logout endpoint",
+        data: "you hit the logout endpoint",
     });
 }
--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,16 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
-const app = express();
-
 dotenv.config();
 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({extended: true})); // to parse from data(urlencoded)
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
     connectMongoDB();
 })
